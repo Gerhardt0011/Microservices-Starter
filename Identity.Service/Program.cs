1a86965ed5d0a0f54d5049e93c3cb3446ad5eafb
Gerhard Theunissen <gerhard@ictglobe.com>
2023-06-08 21:00:17 +0200

    Implmented Health Checks in Identity.Service

--- Identity.Service/Program.cs
+++ Identity.Service/Program.cs
@@ -3,26 +3,30 @@ using Common;
 using Common.MassTransit;
 using Identity.Service.Contracts.Repositories;
 using Identity.Service.Contracts.Services;
+using Identity.Service.HealthChecks;
 using Identity.Service.Models;
 using Identity.Service.Repositories;
 using Identity.Service.Services;
 using Identity.Service.Settings;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
 using Swashbuckle.AspNetCore.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String)); // not sure if this is optimal
-    
+
     var mongoDbSettings = builder.Configuration
         .GetSection(nameof(MongoDbSettings))
         .Get<MongoDbSettings>();
-    
+
     var jwtSettings = builder.Configuration
         .GetSection(nameof(JwtSettings))
         .Get<JwtSettings>();
@@ -34,13 +38,13 @@ var builder = WebApplication.CreateBuilder(args);
         .AddMongoDbStores<ApplicationUser, ApplicationRole, ObjectId>(
             mongoDbSettings!.ConnectionString, mongoDbSettings.DatabaseName
         );
-    
+
     var rsa = RSA.Create();
     rsa.ImportRSAPublicKey(
         source: Convert.FromBase64String(jwtSettings!.PublicKey),
         bytesRead: out var _
     );
-    
+
     var tokenValidatorParameters = new TokenValidationParameters
     {
         ValidateIssuer = false,
@@ -66,7 +70,7 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services.AddMongo();
     builder.Services.RegisterMongoDbCollection<RefreshToken>(mongoDbSettings.RefreshTokensCollectionName);
     builder.Services.RegisterMongoDbCollection<Team>(mongoDbSettings.TeamCollectionName);
-    
+
     builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
     builder.Services.AddScoped<ITeamsRepository, TeamsRepository>();
@@ -84,9 +88,20 @@ var builder = WebApplication.CreateBuilder(args);
             In = ParameterLocation.Header,
             Type = SecuritySchemeType.ApiKey
         });
-        
+
         options.OperationFilter<SecurityRequirementsOperationFilter>();
     });
+
+    builder.Services.AddHealthChecks()
+        .Add(new HealthCheckRegistration(
+            name: "MongoDb",
+            factory: sp => new MongoDbHealthCheck(
+                new MongoClient(mongoDbSettings.ConnectionString)
+            ),
+            failureStatus: HealthStatus.Unhealthy,
+            tags: new[] { "ready" },
+            timeout: TimeSpan.FromSeconds(5)
+        ));
 }
 
 var app = builder.Build();
@@ -97,13 +112,21 @@ var app = builder.Build();
         app.UseSwaggerUI();
     }
 
-    // app.UseHttpsRedirection();
-
     app.UseAuthentication();
-    
+
     app.UseAuthorization();
 
     app.MapControllers();
+
+    app.UseHealthChecks("/health", new HealthCheckOptions
+    {
+        Predicate = _ => false
+    });
+
+    app.UseHealthChecks("/health/ready", new HealthCheckOptions
+    {
+        Predicate = check => check.Tags.Contains("ready"),
+    });
 }
 
 app.Run();
