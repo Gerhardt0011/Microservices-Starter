9d2c347e80d0b666f87a3f545c1cfd32fad7af4f
Gerhard Theunissen <gerhardbotha91@gmail.com>
2023-06-08 10:35:35 +0200

    Indicate that required fields are set on TeamReadDto

--- Teams.Service/Dto/Team/TeamReadDto.cs
+++ Teams.Service/Dto/Team/TeamReadDto.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Common.Enums.Teams;
 using Teams.Service.Dto.User;
 
@@ -5,13 +6,18 @@ namespace Teams.Service.Dto.Team;
 
 public class TeamReadDto
 {
+    [Required]
     public string Id { get; set; } = null!;
 
+    [Required]
     public string UserId { get; set; } = null!;
 
+    [Required]
     public string Name { get; set; } = null!;
 
+    [Required]
     public TeamType Type { get; set; }
 
+    [Required]
     public List<UserReadDto> Members { get; set; } = null!;
 }
