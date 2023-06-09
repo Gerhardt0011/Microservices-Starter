f101c495ba1d7e98178578af0207d0d52e2f040f
Gerhard Theunissen <gerhardbotha91@gmail.com>
2023-06-09 14:34:40 +0200

    Added Basic KeyPairGenerator to Generate Service Keys

---
+++ KeyPairGenerator/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using System.Security.Cryptography;
+
+var rsa = RSA.Create();
+
+var publicKey = rsa.ExportRSAPublicKey();
+var privateKey = rsa.ExportRSAPrivateKey();
+
+Console.WriteLine($"Public key:\n{Convert.ToBase64String(publicKey)}");
+Console.WriteLine($"Private key:\n{Convert.ToBase64String(privateKey)}");
